bc6a119d3228436a3e7cedb29a513a5c25af4f38
WaleyChAn <zerogeneral@foxmail.com>
2023-11-16 14:18:40 +0800

    update: 更新打印样式

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from '@/router'
+import '@/assets/fonts/Noto_Sans/NotoSans.css'
+import '@/assets/fonts/FZHTJW/FZHTJW.css'
 
 Vue.config.productionTip = false
 
